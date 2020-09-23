a7ee06aeba4cb96f22022b28f30e5a4edeb27182
David <music.by.itsmyway@gmail.com>
2020-09-23 20:51:36 +0300

    v1.0

---
+++ #source/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$('.header-slider').slick({
+		dots: true,
+		arrows: false,
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+   		
+	});
+});
+
+
+
+$(document).ready(function(){
+	$('.quote-wrapper').slick({
+		dots: false,
+		arrows: true,
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+   			
+	});
+});
+
+
+$(document).ready(function(){
+	$('.quote-wrapper2').slick({
+		dots: false,
+		arrows: true,
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+   			
+	});
+});
+
+
+
